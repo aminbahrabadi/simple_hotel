e791717a6a754be4f646aaea39bdf5cb73542284
Amin Bahrabadi <amin.bfamily@gmail.com>
2022-01-27 23:36:11 +0330

    Change admin url

--- ettesal_hotel/urls.py
+++ ettesal_hotel/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('roomin/', admin.site.urls),
 
     path('', include('portal.urls')),
     path('accounts/', include('accounts.urls')),
